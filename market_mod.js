a80f7144fa850f193fcc34e7a4f6b25905e31dc0
Bruno <bruno_mussoi@hotmail.com>
2019-08-03 18:33:53 -0300

    new icons plus level 2 markets

--- market_mod.js
+++ market_mod.js
@@ -9,7 +9,7 @@ G.AddData({
 		'market_images':'https://brunosupremo.github.io/market_mod/market_images.png'
 	},
 	func:function() {
-		G.unitCategories.push({
+		G.unitCategories.unshift({
 			id:'market_category',
 			name:'Market'
 		});
@@ -18,7 +18,7 @@ G.AddData({
 			name:'market_tech',
 			displayName:'Markets',
 			desc:'@unlocks [market]s<>[population,Traders] can settle in a piece of [land] to buy or sell items',
-			icon:[0,0,"market_images"],
+			icon:[0,0,"market_images",24,1],
 			cost:{
 				'insight':5,
 				'culture':5
@@ -47,7 +47,7 @@ G.AddData({
 		new G.Trait({
 			name:'traders',
 			desc:'@Your people learn the tricks of trading, prices get 10% better, for both buying and selling',
-			icon:[20,1],
+			icon:[0,1,"market_images",24,1],
 			chance:20,
 			cost: {
 				'culture': 5
@@ -57,25 +57,24 @@ G.AddData({
 
 		new G.Unit({
 			name:'market',
-			desc:'A [population, Trader] can set a market in this piece of [land] to sell or buy items.',
-			icon:[0,0,"market_images"],
+			desc:'A [population, Trader] can sell or buy items.',
+			icon:[0,1,"market_images"],
 			cost:{},
 			req:{'market_tech':true},
 			use:{
 				'worker':1,
-				'land':1,
 			},
 			gizmos:true,
 			modes:{
 				'off':G.MODE_OFF,
 				'buy herb':{
 					name:'buy herbs',
-					icon: [0,0, "market_images", 4,6],
+					icon: [2,0, "market_images", 4,6],
 					desc:'Buy [herb] at 1 [market_coin].'
 				},
 				'sell herb':{
 					name:'sell herbs',
-					icon: [0,0, "market_images", 4,6],
+					icon: [1,0, "market_images", 4,6],
 					desc:'Sell [herb] for 0.5 [market_coin].'
 				},
 			},
@@ -91,6 +90,56 @@ G.AddData({
 				type:'convert',
 				from:{'herb':1},
 				into:{'market_coin':0.5},
+				every:5,
+				mode:'sell herb'
+			},
+			{
+				type:'mult',
+				value:1.1,
+				req:{
+					'traders':true
+				}
+			}
+			],
+			category:'market_category',
+		});
+
+		new G.Unit({
+			name:'market2',
+			desc:'A [population, Trader] can set a market in this piece of [land] to sell or buy items in bulks of 10 at once.',
+			icon:[1,1,"market_images"],
+			cost:{},
+			req:{'market_tech':true},
+			use:{
+				'worker':1,
+				'land':1,
+			},
+			gizmos:true,
+			modes:{
+				'off':G.MODE_OFF,
+				'buy herb':{
+					name:'buy herbs',
+					icon: [2,0, "market_images", 4,6],
+					desc:'Buy [herb] at 1 [market_coin].'
+				},
+				'sell herb':{
+					name:'sell herbs',
+					icon: [1,0, "market_images", 4,6],
+					desc:'Sell [herb] for 0.5 [market_coin].'
+				},
+			},
+			effects:[
+			{
+				type:'convert',
+				from:{'market_coin':10},
+				into:{'herb':10},
+				every:5,
+				mode:'buy herb'
+			},
+			{
+				type:'convert',
+				from:{'herb':10},
+				into:{'market_coin':5},
 				every:5,
 				mode:'sell herb'
 			},
